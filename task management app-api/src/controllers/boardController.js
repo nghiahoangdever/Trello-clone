bca6da14c60d8d183421286a4c7b15b7bc14caec
nghiahoangdever <hoangdainghia1818@gmail.com>
2026-01-28 10:43:13 +0700

    Add backend code

---
+++ task management app-api/src/controllers/boardController.js
@@ -0,0 +1,70 @@
+import { StatusCodes } from 'http-status-codes'
+import { boardService } from '~/services/boardService'
+
+const createNew = async (req, res, next) => {
+  try {
+    // console.log('req.body: ', req.body)
+    // console.log('req.query: ', req.query)
+    // console.log('req.params: ', req.params)
+    // console.log('req.files: ', req.files)
+    // console.log('req.cookies: ', req.cookies)
+    // console.log('req.jwtDecoded: ', req.jwtDecoded)
+
+    const userId = req.jwtDecoded._id
+
+    // Điều hướng dữ liệu sang tầng Service
+    const createdBoard = await boardService.createNew(userId, req.body)
+
+    // Có kết quả thì trả về phía Client
+    res.status(StatusCodes.CREATED).json(createdBoard)
+  } catch (error) { next(error) }
+}
+
+const getDetails = async (req, res, next) => {
+  try {
+    const userId = req.jwtDecoded._id
+    const boardId = req.params.id
+    const board = await boardService.getDetails(userId, boardId)
+    res.status(StatusCodes.OK).json(board)
+  } catch (error) { next(error) }
+}
+
+const update = async (req, res, next) => {
+  try {
+    const boardId = req.params.id
+    const updatedBoard = await boardService.update(boardId, req.body)
+
+    res.status(StatusCodes.OK).json(updatedBoard)
+  } catch (error) { next(error) }
+}
+
+const moveCardToDifferentColumn = async (req, res, next) => {
+  try {
+    const result = await boardService.moveCardToDifferentColumn(req.body)
+
+    res.status(StatusCodes.OK).json(result)
+  } catch (error) { next(error) }
+}
+
+const getBoards = async (req, res, next) => {
+  try {
+    const userId = req.jwtDecoded._id
+    // page và itemsPerPage được truyền vào trong query url từ phía FE nên BE sẽ lấy thông qua req.query
+    const { page, itemsPerPage, q } = req.query
+    const queryFilters = q
+    // console.log(queryFilters)
+
+    const results = await boardService.getBoards(userId, page, itemsPerPage, queryFilters)
+
+    res.status(StatusCodes.OK).json(results)
+  } catch (error) { next(error) }
+}
+
+
+export const boardController = {
+  createNew,
+  getDetails,
+  update,
+  moveCardToDifferentColumn,
+  getBoards
+}
---
+++ task management app-api/src/routes/v1/index.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { boardRoute } from '~/routes/v1/boardRoute'
+import { columnRoute } from '~/routes/v1/columnRoute'
+import { cardRoute } from '~/routes/v1/cardRoute'
+import { userRoute } from '~/routes/v1/userRoute'
+import { invitationRoute } from '~/routes/v1/invitationRoute'
+
+const Router = express.Router()
+
+/** Check APIs v1/status */
+Router.get('/status', (req, res) => {
+  res.status(StatusCodes.OK).json({ message: 'APIs V1 are ready to use.' })
+})
+
+/** Board APIs */
+Router.use('/boards', boardRoute)
+
+/** Column APIs */
+Router.use('/columns', columnRoute)
+
+/** Cards APIs */
+Router.use('/cards', cardRoute)
+
+/** User APIs */
+Router.use('/users', userRoute)
+
+/** Invitation APIs */
+Router.use('/invitations', invitationRoute)
+
+export const APIs_V1 = Router
